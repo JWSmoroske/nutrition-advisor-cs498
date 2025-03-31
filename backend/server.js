62ef8df7318569278f4606a342d4a2083d2a593f
Jack Smoroske <jwsmoroske@gmail.com>
2025-03-31 19:01:05 -0400

    added node packages for mysql, import into server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import OpenAI from 'openai'; // Changed import
+import { insertEntity, updateEntity, deleteEntity, getAllEntities } from 'databaseAccess.js';
 
 dotenv.config();
 
