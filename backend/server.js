5052e4dbb70a2e00459590b3b19eee8318795dff
ytabita23 <105095589+ytabita23@users.noreply.github.com>
2025-03-31 20:13:16 -0400

    Fixed with import statement for the db
    changed the import statement associated with the db to reflect how they are called

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import OpenAI from 'openai'; // Changed import
-import { insertEntity, updateEntity, deleteEntity, getAllEntities } from './databaseAccess.js';
+import * as db from './databaseAccess.js'; // import in databaseAccess file to call functions
 
 dotenv.config();
 
